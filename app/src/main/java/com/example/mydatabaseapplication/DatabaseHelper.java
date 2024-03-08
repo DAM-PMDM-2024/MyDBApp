37c1d78d9129c473812e5b9e87592e180b9d57a3
Jotivirix <joignasa@gmail.com>
2024-03-08 15:11:44 +0100

    First commit

---
+++ app/src/main/java/com/example/mydatabaseapplication/DatabaseHelper.java
@@ -0,0 +1,34 @@
+package com.example.mydatabaseapplication;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private Context myContext;
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, "MyLibrary.db", null, 1);
+        this.myContext = context;
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String query = "CREATE TABLE Books" +
+                "(id INTEGER PRIMARY KEY AUTOINCREMENT,"+
+                "title TEXT NOT NULL,"+
+                "author TEXT NOT NULL,"+
+                "pages INTEGER)";
+        sqLiteDatabase.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+}
---
+++ app/src/main/java/com/example/mydatabaseapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.mydatabaseapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dbHelper = new DatabaseHelper(MainActivity.this);
+
+    }
+}
